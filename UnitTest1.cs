a6ceff6d5e36b388a44d111a7936594641853b23
Matt Kellock <matt@kellock.com.au>
2020-12-01 10:27:13 +1100

    Numerous changes

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -5,61 +5,77 @@ using FsCheck.Xunit;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Globalization;
+using Xunit.Abstractions;
+using NLog;
 
 namespace FsCheckDemo
 {
-    public class Input
+    public class SomeInput
     {
-        public string input1 { get; set; }
-        public int input2 { get; set; }
-        public bool input3 { get; set; }
-        public Guid input4 { get; set; }
-        public decimal input5 { get; set; }
+        public string Input1 { get; set; }
+        public int Input2 { get; set; }
+        public bool Input3 { get; set; }
+        public Guid Input4 { get; set; }
+        public decimal Input5 { get; set; }
     };
 
     public interface ISomeDependency {
-        string DependencyFunction(Input input);
+        string DependencyFunction(SomeInput input);
     }
 
     public class SomeDependency : ISomeDependency {
-        public string DependencyFunction(Input input) {
-            return input.input1 + input.input2.ToString() + input.input3.ToString() + input.input4.ToString() + input.input5.ToString();
+        public string DependencyFunction(SomeInput input) {
+            // Throw an exception if null
+            if (input == null)
+                throw new ArgumentNullException(nameof(input));
+
+            return input.Input1 + 
+                input.Input2.ToString(CultureInfo.CurrentCulture) + 
+                input.Input3.ToString() + 
+                input.Input4.ToString() + 
+                input.Input5.ToString(CultureInfo.CurrentCulture);
         }
     }
 
-    public class SomeApp {
-        public string SomeFunction(Input input, ISomeDependency someDependency) {
+    public static class SomeApp {
+        public static string SomeFunction(SomeInput input, ISomeDependency someDependency) {
+            // Throw an exception if null
+            if (someDependency == null)
+                throw new ArgumentNullException(nameof(someDependency));
+
             return someDependency.DependencyFunction(input);
         }
     }
 
     public class InputGenerators
     {
-        public static Arbitrary<Input> InputGenerator()
+        public static Arbitrary<SomeInput> InputGenerator()
         {
             return (from x in Gen.Choose(0, 1024)
                     from y in Arb.Generate<string>()
-                    select new Input
+                    select new SomeInput
                     {
-                        input1 = y,
-                        input2 = x,
-                        input3 = x % 2 == 0,
-                        input4 = Guid.NewGuid(),
-                        input5 = (decimal)(x / 2.0)
+                        Input1 = y,
+                        Input2 = x,
+                        Input3 = x % 2 == 0,
+                        Input4 = Guid.NewGuid(),
+                        Input5 = (decimal)(x / 2.0)
                     }).ToArbitrary();
         }
     }
 
-    public class UnitTests
+    public class UnitTests : IDisposable
     {
+        private readonly Logger _logger = LogManager.GetCurrentClassLogger();
 
-        public UnitTests() {
+        public UnitTests(ITestOutputHelper output)
+        {
+
             var configuration = Configuration.Default;
-            configuration.MaxNbOfTest = 1000000;
-            configuration.QuietOnSuccess = true;
             true.ToProperty().Check(configuration);
 
-            Arb.Register<FsCheckDemo.InputGenerators>();
+            Arb.Register<InputGenerators>();
         }
 
         public static IEnumerable<object[]> TestData()
@@ -68,54 +84,80 @@ namespace FsCheckDemo
             {
                 yield return  new object[]
                 {
-                    new Input
+                    new SomeInput
                     {
-                        input1 = "", // Provide a random string
-                        input2 = i, // Provide an integer
-                        input3 = i % 2 == 0, // Provide a bool
-                        input4 = Guid.Empty, // Provide a guid
-                        input5 = (decimal)(i / 2.0) // Provide a decimal
+                        Input1 = "", // Provide a random string
+                        Input2 = i, // Provide an integer
+                        Input3 = i % 2 == 0, // Provide a bool
+                        Input4 = Guid.Empty, // Provide a guid
+                        Input5 = (decimal)(i / 2.0) // Provide a decimal
                     }
                 };
             }
         }
 
-        [Property]
-        public void PropertyTest(Input input)
+        [Property(MaxTest = 100000)]
+        public void PropertyTest(SomeInput input)
         {
+            // Throw an exception if null
+            if (input == null)
+                throw new ArgumentNullException(nameof(input));
+
+            _logger.Info(input.Input2.ToString(CultureInfo.CurrentCulture));
+
             // Run the test as a property based test
             Test(input);
         }
 
         [Theory]
         [MemberData(nameof(TestData))]
-        public void RangeTest(Input input)
+        public void RangeTest(SomeInput input)
         {
+            // Throw an exception if null
+            if (input == null) 
+                throw new ArgumentNullException(nameof(input));
+
+            _logger.Info(input.Input2.ToString(CultureInfo.CurrentCulture));
+
             // Run the test as a theory
-            Test(input);
+            var ex = Record.Exception(() => Test(input));
+            Assert.Null(ex);
         }
 
-        private void Test(Input input)
+        private static void Test(SomeInput input)
         {
             // Create the mock of FsCheck.ISomeDependency
-            Mock<FsCheckDemo.ISomeDependency> dependencyMock = new Mock<FsCheckDemo.ISomeDependency>();
+            Mock<ISomeDependency> dependencyMock = new Mock<ISomeDependency>();
             
             // Default behaviour is that the mock works fine
             dependencyMock.Setup(
-                x => x.DependencyFunction(It.IsAny<Input>())
+                x => x.DependencyFunction(It.IsAny<SomeInput>())
                 ).Returns("Working!");
 
+            /*
             // If input 2 = 666 then throw an exception
             dependencyMock.Setup(
-                x => x.DependencyFunction(It.Is<Input>(x => x.input2 == 666)
+                x => x.DependencyFunction(It.Is<SomeInput>(x => x.Input2 == 666)
                 )
             ).Throws(
                     new System.Exception("The power of Christ compeles thee!")
                     );
+            */
 
             // Create an instance of FsCheck.SomeApp and inject the mock
-            FsCheckDemo.SomeApp someApp = new FsCheckDemo.SomeApp();
-            Assert.True(someApp.SomeFunction(input, dependencyMock.Object) == "Working!");
+            Assert.True(SomeApp.SomeFunction(input, dependencyMock.Object) == "Working!");
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            // Cleanup
+            LogManager.Shutdown();
         }
     }
 }
